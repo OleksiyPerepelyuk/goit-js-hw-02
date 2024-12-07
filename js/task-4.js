3b506ae142e0154f95bc7f6d3926903a7b0fa3eb
AlexP <alexperepelyuk@gmail.com>
2024-12-07 13:38:36 +0100

    added task

---
+++ js/task-4.js
@@ -0,0 +1,8 @@
+function makeMessage(name, price) {
+    return `You picked ${name}, price per item is ${price} credits!`
+}
+
+console.log(makeMessage('Radar', 6150))
+console.log(makeMessage('Scanner', 3500))
+console.log(makeMessage('Reactor', 8000))
+console.log(makeMessage('Engine', 4070))

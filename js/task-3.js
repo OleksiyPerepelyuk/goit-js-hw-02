876cb9b15da298e444087d934882a803f11599d4
AlexP <alexperepelyuk@gmail.com>
2024-12-08 12:58:30 +0100

    added task-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,13 @@
 function checkForSpam(message) {}
+
+function checkPassword(password) {
+    const correctPassword = 'jqueryismyjam'
+
+    password = correctPassword
+        ? `Access granted`
+        : `Access denied, wrong password!`
+}
+
+console.log(checkPassword('jqueryismyjam'))
+console.log(checkPassword('angul4r1sl1f3'))
+console.log(checkPassword('r3actsux'))
